8f941952b67e3ecdfd0da53ea11b69766ba62d47
Niponpal <niponpal52@gmail.com>
2025-12-15 11:20:59 +0600

    Fix 🚀🚀🚑 ApplicationDbContext

---
+++ BankMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BankMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankMS.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+
+    }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<AuditLog> AuditLogs { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Card> Cards { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<FundTransfer> FundTransfers { get; set; }  
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }    
+    public DbSet<User> Users { get; set; }
+}
--- BankMS/obj/Debug/net9.0/BankMS.AssemblyInfo.cs
+++ BankMS/obj/Debug/net9.0/BankMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cff8401c91c2f61b19c66b112b267f963ad9e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
