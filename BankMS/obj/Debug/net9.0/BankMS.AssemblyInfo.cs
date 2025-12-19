3afb2b862eb7926f17d290c959eac26dc28a686a
Niponpal <niponpal52@gmail.com>
2025-12-20 00:17:53 +0600

    Fix 🚀🚀🚑

--- BankMS/obj/Debug/net9.0/BankMS.AssemblyInfo.cs
+++ BankMS/obj/Debug/net9.0/BankMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9681f636f866108136aabce27377e80b961046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc45e384f1a3420cfe5d4194a1614b9029c9dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
