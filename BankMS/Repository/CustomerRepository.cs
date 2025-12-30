d4a97fab70e46b2fda0980d92cdeef89fa7056aa
Niponpal <niponpal52@gmail.com>
2025-12-30 17:05:27 +0600

    Fix CustomerRepository 🚀🚀🚑

--- BankMS/Repository/CustomerRepository.cs
+++ BankMS/Repository/CustomerRepository.cs
@@ -1,5 +1,64 @@
-﻿namespace BankMS.Repository;
+﻿using BankMS.Data;
+using BankMS.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class CustomerRepository
+namespace BankMS.Repository;
+
+public class CustomerRepository : ICustomerRepository
 {
+    private readonly ApplicationDbContext _context;
+    public CustomerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Customer> GetAddCustomerAsynce(Customer customer)
+    {
+        await _context.Customers.AddAsync(customer);
+        await _context.SaveChangesAsync();
+        return customer;
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllCustomerAsync()
+    {
+        var customers = await _context.Customers.ToListAsync();
+        return customers;
+    }
+
+    public async Task<Customer> GetCustomerByIdAsync(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer != null)
+        {
+            return customer;
+        }
+        return null!;
+    }
+
+    public async Task<Customer> GetDeleteCustomerAsync(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer != null)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return customer;
+        }
+        return null!;
+    }
+
+    public async Task<Customer> GetUpdateCustomerAsync(Customer customer)
+    {
+        var existingCustomer = await _context.Customers.FindAsync(customer.Id);
+        if (existingCustomer != null)
+        {
+            existingCustomer.NIDNumber = customer.NIDNumber;
+            existingCustomer.Address = customer.Address;
+            existingCustomer.DateOfBirth = customer.DateOfBirth;
+            existingCustomer.KYCStatus = customer.KYCStatus;
+            existingCustomer.Gender = customer.Gender;
+            await _context.SaveChangesAsync();
+            return existingCustomer;
+        }
+        return null!;
+    }
 }
